97f460a8687b0a7c0c54c27d734b9dd09ce3cef7
Vinicius <vini82001@gmail.com>
2024-04-26 01:26:29 -0300

    add-challenge/DesafioControleFluxo

---
+++ DesafioControleFluxo/src/Contador.java
@@ -0,0 +1,41 @@
+
+import java.util.Scanner;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+/**
+ *
+ * @author Vinicius
+ */
+public class Contador {
+
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro");
+        int parametroUm = terminal.nextInt();
+        System.out.println("Digite o segundo parâmetro");
+        int parametroDois = terminal.nextInt();
+
+        try {
+            contar(parametroUm, parametroDois);
+
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());
+        }
+
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        if (parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
+        }
+
+        int contagem = parametroDois - parametroUm;
+
+        for (int i = 0; i < contagem; i++) {
+            System.out.println("Imprimindo o número " + (i + 1));
+        }
+    }
+}
