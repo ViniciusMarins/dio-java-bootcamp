f63be14b20ba189c9cd7f1d17cb2262d3b3754d5
Vinicius <vini82001@gmail.com>
2024-05-12 22:57:22 -0300

    uml iphone challenge

---
+++ iPhone-uml/src/NavegadorInternet.java
@@ -0,0 +1,5 @@
+public interface NavegadorInternet {
+    void adicionarNovaAba();
+    void exibirPagina();
+    void atualizarPagina();
+}
---
+++ iPhone-uml/src/iPhone.java
@@ -0,0 +1,47 @@
+public class iPhone implements AparelhoTelefonico, NavegadorInternet, ReprodutorMusical{
+
+    @Override
+    public void ligar() {
+
+    }
+
+    @Override
+    public void atender() {
+
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+
+    }
+
+    @Override
+    public void exibirPagina() {
+
+    }
+
+    @Override
+    public void atualizarPagina() {
+
+    }
+
+    @Override
+    public void tocar() {
+
+    }
+
+    @Override
+    public void pausar() {
+
+    }
+
+    @Override
+    public void selecionarMusica() {
+
+    }
+}
